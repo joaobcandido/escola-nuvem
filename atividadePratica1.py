d260e9dc8f8dfe3cf72e12328e5565e71f5c7ba6
joao candido <61605388+joaobcandido@users.noreply.github.com>
2025-06-17 21:18:53 -0300

    exercicio : atividade pratica 1

---
+++ atividadePratica1.py
@@ -0,0 +1,27 @@
+#Programa de Saudação
+print("Olá, mundo!")
+
+
+#Calculadora de Soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma é:", soma)
+
+#Calculadora de Volume
+comprimento = 12  
+largura = 14      
+altura = 20      
+volume = comprimento * largura * altura
+print("O volume da caixa é:", volume, "cm³")
+
+#Calculadora de Preço Total
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
